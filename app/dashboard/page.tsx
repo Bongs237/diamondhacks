9271181441725de12a6a0ad23fc0c3c923483eb6
Bongs237 <10395903+Bongs237@users.noreply.github.com>
2026-04-04 12:31:45 -0700

    Add dummy page.tsx and fastapi app in init.py

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>I'm dashing all over the board, call that a dashboard?</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = () => {
+    console.log(phoneNumber);
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-5xl">hi</h1>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
+      <h1 className="text-3xl font-bold">To start, enter your phone number below</h1>
+
+      <div className="flex flex-row gap-2 pt-5">
+        <input
+          type="tel"
+          pattern="[0-9]*"
+          placeholder="Phone Number"
+          className="border-2 border-gray-300 rounded-md p-2"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <button className="bg-blue-500 text-white p-2 rounded-md" onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 }
